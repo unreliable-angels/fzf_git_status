ed829a90c32260faecfbc435c2d062b5db6b6366
Hirofumi Wakasugi <baenej@gmail.com>
2017-05-01 11:26:02 +0900

    Warn when not in git repo

--- functions/fzf_git_status.fish
+++ functions/fzf_git_status.fish
@@ -2,6 +2,8 @@ function fzf_git_status -d "fzf source to select from git status"
     git rev-parse --is-inside-work-tree >/dev/null ^/dev/null
 
     if not test $status -eq 0
+        command echo 'fzf_git_status: Not a git repository'
+        commandline -f execute
         return
     end
 
